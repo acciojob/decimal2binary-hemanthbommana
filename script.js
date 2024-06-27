4006852e6ab78da32d71f08a1002f831aa09c2ac
hemanthbommana <117904125+hemanthbommana@users.noreply.github.com>
2024-06-27 12:42:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function decimalToBinary(num) {
   //Write you code here
+	 var str = "";
+    while (num) 
+    {
+    
+        // adding the result of num & 1 to str
+        if (num & 1) // 1
+            str += '1';
+        else // 0
+            str += '0';
+            
+        // Right Shift by 1
+        num >>= 1;
+    }
+    return str;
   
 }
 
